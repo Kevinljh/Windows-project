c66a697c75572405e8d9d4da21cdc5f4cc882876
mldasilva <mldasilva-cc@conestogac.on.ca>
2014-12-02 15:13:58 -0500

    Setup basic ui Renamed Form.cs github deleted it

--- Project/Project/ServerForm.cs
+++ Project/Project/ServerForm.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace Project
 {
-    public partial class Form1 : Form
+    public partial class ServerForm : Form
     {
-        public Form1()
+        public ServerForm()
         {
             InitializeComponent();
         }
+
     }
 }
